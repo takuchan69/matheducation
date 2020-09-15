17f6a1520bbb1db4dd6a21c15eb09d671e4a05a0
takuchan69 <57449179+takuchan69@users.noreply.github.com>
2020-09-15 13:52:01 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang='ja'>
+       <head>
+         <meta charset='UTF-8'>
+         <meta name='viewport' content='width=device-width'>
+         <meta name='keywords' content='数学、医学部、大学受験、難関大学、プログラミング教育'>
+         <title>学習舎</title>
+         <link rel='stylesheet' href='styles.css'>
+       </head>
+       <body>
+         <header>
+               <h1>学習舎のオンライン数学講座</h1>
+         </header>
+         <section id='container'>
+         　　　　　<h3>オンラインでも成績を伸ばすには・・・</h3>
+              　　<ul>
+                　　　<li>出題される問題タイプを全て習うこと</li>
+                     <li>類題を「実際に自分で解いてみること」</li>
+                     <li>復習すること（身につけることですね）</li>
+                 </ul>
+         </section>
+         <footer>
+         　　　　　<h3>学習舎の数学講座 &rquto; &copy; &left;</h3>
+         </footer>
+       </body>
+</html>

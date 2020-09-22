c1a91e38791368bc21b575d6e3fbe1257068a259
takuchan69 <taku.n@find18.net.in>
2020-09-22 13:20:25 +0900

    22/sept/2020

--- syllabus.php
+++ syllabus.php
@@ -10,6 +10,18 @@
           <link rel='stylesheet' href='styles.css'>
         </head>
         <body>
-          
+          <header>
+          </header>
+          <div id='toolbar'>
+                 <section class='toolbar_name'><a href='lessonstyle.php'>講座の受講style</a></section>
+                 <section class='toolbar_name'><a href='syllabus.php'>講座の内容</a></section>
+                 <section class='toolbar_name'><a href='trial.php'>体験授業</a></section>
+          </div>
+          <section id='container'>
+          </section>
+          <footer>
+             <h3>学習舎の数学講座 &lt; &copy; &gt;</h3>
+          </footer>
+
         </body>
  </html>
--- trial.php
+++ trial.php
@@ -13,6 +13,11 @@
          <header>
                <h1> How to try lesson</h1>
          </header>
+         <div id='toolbar'>
+                <section class='toolbar_name'><a href='lessonstyle.php'>講座の受講style</a></section>
+                <section class='toolbar_name'><a href='syllabus.php'>講座の内容</a></section>
+                <section class='toolbar_name'><a href='trial.php'>体験授業</a></section>
+         </div>
          <section id='container'>
                 <ul>
                     <li class='list'>mail or phone for trial day</li>
@@ -36,6 +41,7 @@
                 </form>
          </section>
          <footer>
+                <h3>学習舎の数学講座 &lt; &copy; &gt;</h3>
          </footer>
 
        </body>

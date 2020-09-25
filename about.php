9cb66d1e6c3c576786fc63a3da51d0cfd96a1d54
takuchan69 <57449179+takuchan69@users.noreply.github.com>
2020-09-25 17:33:42 +0900

    Update about.php

--- about.php
+++ about.php
@@ -20,7 +20,16 @@
 
 
             <section id='container'>
-                <article class='box'></box>
+                <article class='box'>
+                      <h3>講師紹介</h3>
+                       永田　卓：1969年生まれ
+                       　　　　　市進予備校に始まり大手予備校などで数学の受験指導に当たること３０年ほど。
+                       　　　　　現在数学力を向上させる方法を柱にオンラインでも個別指導にあたる。
+                       　　　　　最近は数学の周辺の教科や科目の指導にも関心が広がり、中学受験、高校受験
+                       　　　　　の数学や理科の指導、プログラミングの指導にも当たる。
+                </article>
+                <article class='box'>
+                </article>
             </section>
             <footer>
                 <h3>学習舎の数学講座 &lt; &copy; &gt;</h3>

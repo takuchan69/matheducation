3c02824c8e3628de5091918d2f807d50352b068d
takuchan69 <taku.n@find18.net.in>
2020-10-10 18:32:13 +0900

    howto_zoom.php is made.

---
+++ howto_zoom.php
@@ -0,0 +1,41 @@
+<?php
+
+ ?>
+ <!DOCTYPE html>
+ <html lang='ja'>
+        <head>
+              <meta charset='utf-8'>
+              <meta name='description' content='Introduce how to use Zoom application'>
+              <meta name='viewport' content='width=device-width'>
+              <title>How to use Zoom</title>
+              <link rel='stylesheet' href='styles.css'>
+        </head>
+        <body>
+              <h2>How to use Zoom application</h1>
+                <dl>
+                    <dt>Go to Zoom application site it's free!</dt>
+                    <dd></dd>
+                    <dt>press sign up button on left top</dt>
+                    <dd>1.input your birth to make sure you are not robot.<br>
+                        2.input your email address,then press sign up button.<br>
+                        3.You should recieve an email from Zoom.<br>
+                        4.validate at email you have recieved.
+                    </dd>
+                    <dt>Login in and start join meeting</dt>
+                    <dd>1.Login to your account<br>
+                        2.start press when your invited to meeting.<br>
+                        3.press sound for internet.<br>
+                    </dd>
+                    <dt>download app in your device</dt>
+                    <dd>1.go to apple store and find Zoom<br>
+                        2.try install application.
+                    </dd>
+                    <dt>download this introduction</dt>
+                    <dd> <a href='***.pdf' download='***.pdf'>downLoads</a></dd>
+                    <dt>visit Zoom site at us</dt>
+                    <dd><a href='https://zoom.us' target='blank'>link to Zoom.us</a></dd>
+                </dl>
+
+
+        </body>
+ </html>
--- method.php
+++ method.php
@@ -18,22 +18,22 @@
                     <section class='toolbar_name'><a href='syllabus.php'>講座の内容</a></section>
                     <section class='toolbar_name'><a href='trial.php'>体験授業</a></section>
              </div>
-             <section id='parent'>
+             <section id='container'>
                     <h3>学習舎の方法</h3>
                     数学に限らず「できるために必要なこと」は昔よりそんなに変わるわけではありません。
                     それは「自分で習ったことを使ってみる」ということです。つまり、「聞いてるだけでなく自分で
                     解いてみること」こそ成績向上のポイントです。
                     　学習舎ではそんな成績向上の仕組みをオンラインで提供しています。
                     <ul >
-                           <li class='list'>動画で予習(10分ほどです)</li>
-                           <li class='list'>Zoomで個別指導(ライブで40分ほどです)</li>
-                           <li class='list'>課題を後日提出(自宅などで解いて答案の写真を送ってもらいます）</li>
+                          <li class='list'>動画で予習(10分ほどです)</li>
+                          <li class='list'>Zoomで個別指導(ライブで40分ほどです)</li>
+                          <li class='list'>課題を後日提出(自宅などで解いて答案の写真を送ってもらいます）</li>
                     </ul>
-                    <ul>
-                           <dt>受講にあたりZoomが使用できる環境が必要です</dt>
-                           <dd>タブレットもしくはスマートフォンが必要です</dd>
+                    <dl>
+                           <dt class='list'>受講にあたりZoomが使用できる環境が必要です</dt>
+                           <dd class='list'>タブレットもしくはスマートフォンが必要です</dd>
                            <a href='howto_zoom.php'　class='howto_zoom'>Zoomの使い方はここをクリックしてください</a>
-                    </ul>
+                    </dl>
              </section>
              <footer>
                <h3>学習舎の数学講座 &lt; &copy; &gt;</h3>

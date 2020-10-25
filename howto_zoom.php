634d2ba5d4734f525df3246a7d3dfdeceb75843c
takuchan69 <57449179+takuchan69@users.noreply.github.com>
2020-10-25 19:35:28 +0900

    Update howto_zoom.php

--- howto_zoom.php
+++ howto_zoom.php
@@ -11,28 +11,32 @@
               <link rel='stylesheet' href='styles.css'>
         </head>
         <body>
-              <h2>How to use Zoom application</h1>
+              <h2>個別指導で用いるアプリZoomの使用方法</h2>
+        　　　 <h4>(いずれの使い方でも費用はかかりません)</h4>
                 <dl>
-                    <dt>Go to Zoom application site it's free!</dt>
-                    <dd></dd>
-                    <dt>press sign up button on left top</dt>
-                    <dd>1.input your birth to make sure you are not robot.<br>
-                        2.input your email address,then press sign up button.<br>
-                        3.You should recieve an email from Zoom.<br>
-                        4.validate at email you have recieved.
+                    <dt><strong>1.Zoomをお使いの環境に入れる</strong></dt>
+                   <dd>(いずれも授業を受けるだけであれば特別に情報の入力は求められません</dd>
+                   <dt><strong>スマホ・タブレットに入れる場合</strong></dt>
+                    <dd>アップルストアなどでZoomを検索して探せばみつかります。無料で入れることができます。</dd>
+                 <dt><strong>パソコンで使用する場合</strong></dt>
+                 <dd>公式サイトである<a href='https://zoom.us'>zoom.us</a>にアクセスします。<br>
+                    ページの一番下にある「ダウンロード」部の「ミーティングクライアント」の部分をクリックします。<br>
+                    ページがダウンロードページになったら「ミーティング用Zoomクライアント」のボタンを押すと自動的にパソコンに<br>
+                    インストールが始まります。<br>
+                  　特段の情報入力はありませんので、ダウンロードとインストールが終わるのを待ちます。<br>
+                   インストールが終わると自動的に使えるようになります。<br>
+                  
+                 <dt><strong>Zoomを入れたあとの使い方</strong></dt>
+                    <dd>1.予め授業開始時にメールを送ります。<br>
+                     2.メールには授業に参加するための<strong>ミーティングIDとパスコードがあるのでこれを控えておきましょう。</strong><br>
+                        3.URLをクリックするとZoomのアプリが始まりミーティングIDとパスコードの入力を求められるので入力してください。<br>
+                        4.授業開始時前は「控室」にいる状態で、授業開始とともに自動的に参加します。<br>
+                     　 5. 授業が終わったらZoomの画面にある「退出」ボタンを押すと自動的に終了します。<br>
                     </dd>
-                    <dt>Login in and start join meeting</dt>
-                    <dd>1.Login to your account<br>
-                        2.start press when your invited to meeting.<br>
-                        3.press sound for internet.<br>
-                    </dd>
-                    <dt>download app in your device</dt>
-                    <dd>1.go to apple store and find Zoom<br>
-                        2.try install application.
-                    </dd>
-                    <dt>download this introduction</dt>
-                    <dd> <a href='***.pdf' download='***.pdf'>downLoads</a></dd>
-                    <dt>visit Zoom site at us</dt>
+                    
+                    <dt>Zoomの使い方の案内をダウンロードする</dt>
+                    <dd> <a href='howtoZoom.pdf' download='howtoZoom.pdf'>ここをクリックするとダウンロードが始まります。</a></dd>
+                    <dt>Zoomの公式サイトを見る</dt>
                     <dd><a href='https://zoom.us' target='blank'>link to Zoom.us</a></dd>
                 </dl>
 

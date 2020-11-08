45d90922e584610a909118afc46cf0a9bf0fcf60
takuchan69 <taku.n@find18.net.in>
2020-11-08 15:55:50 +0900

    trial.php changed

--- trial.php
+++ trial.php
@@ -3,15 +3,6 @@
 include_once('');
 $today = new DateTime('today');
 
-$this_month = new DatePeriod(new DateTime('first day of this month'),
-                             new DateInterval('P1D'),
-                             new DateTime('first day of next month'));
-$next_month = new DatePeriod(new DateTime('first day of next month'),
-                             new DateInterval('P1D'),
-                             new DateTime('first day of next month +1 month'));
-foreach($this_month as $day){
-   echo $day->format('Y-m-d').'    ';
-}
 
 ?>
 <!DOCTYPE html>
@@ -41,78 +32,7 @@ foreach($this_month as $day){
                 <form method='post' action='trial_post.php' >
                     <label>受講希望日程を以下より選択してください<strong>必須</strong>
                            <h3><?=$today->format('Y').'year'.$today->format('m').'月';?></h3>
-                           <table class='trial_table'>
-                                  <thead>
-                                         <th class='sunday'>日</th>
-                                         <th class='weekday'>月</th>
-                                         <th class='weekday'>火</th>
-                                         <th class='weekday'>水</th>
-                                         <th class='weekday'>木</th>
-                                         <th class='weekday'>金</th>
-                                         <th class='saturday'>土</th>
-                                  </thead>
-                                  <tr>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td>1</td>
-                                         <td>2</td>
-                                         <td>3</td>
-                                  </tr>
-                                  <tr>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                  </tr>
-                                  <tr>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                  </tr>
-                                  <tr>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                  </tr>
-                                  <tr>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                         <td></td>
-                                  </tr>
-                           </table>
-                      <input type='date' placeholder='８月２２日'>
-                      <input type='time' placeholder='17:00~'>
-                    </label><br><br>
-                    <label>お名前<strong>必須</strong>
-                      <input type='text' placeholder='ex)山田　太郎'>
-                    </label><br><br>
-                    <label>学年<strong>必須</strong>
-                      <input type='number' placeholder='高校２年'>
-                    </label><br><br>
-                    <label>電話番号（ご連絡先)<strong>必須</strong>
-                      <input type='text' placeholder='03-2345-＊＊＊＊'>
-                    </label><br><br>
-                    <label>e-mail（ご連絡先)<strong>必須</strong>
-                      <input type='text' placeholder='gakushusha@email.com'>
-                    </label><br><br>
-                    <input type='submit' value='send'>
+
                 </form>
                 <h3 class='goto_index.php'><a href='index.php'>トップページに戻る</a></h3>
          </section>

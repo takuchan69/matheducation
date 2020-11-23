bddbe7f8e092d6aced4cc0a728eca5398f49ae74
takuchan69 <57449179+takuchan69@users.noreply.github.com>
2020-11-23 15:34:28 +0900

    Update send_method.php

--- send_method.php
+++ send_method.php
@@ -13,11 +13,13 @@ namespace send_message;
         $this->_mail = $_POST['mail'];
         $this->_reason = $_POST['reason'];
         $this->_grade = $_POST['grade'];
+        $this->_contact1 = $_POST['requestno1'];
+         $this->_contact2 = $_POST['requestno2'];
          if($this->_name === '' || $this->_tel === '' || $this->_mail === ''){
-           return  'name of must should not be empty';
+           return  '「必須」項目に未記入の項目があります';
          }else{
            $this->_date = $_POST['date'];
-           $_SESSION['contact_data'] = [$this->_name,$this->_tel,$this->_mail,$this->_reason,$this->_grade,$this->_date];
+           $_SESSION['contact_data'] = [$this->_name,$this->_tel,$this->_mail,$this->_reason,$this->_grade,$this->_date,$this->_contact1,$this->_contact2];
            header('Location:show_input.php');
 
          }

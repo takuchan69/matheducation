46001b4555027d5c91ee0b5c2edb29141486e3a6
takuchan69 <57449179+takuchan69@users.noreply.github.com>
2020-12-24 11:09:02 +0900

    Update trial.php

--- trial.php
+++ trial.php
@@ -18,7 +18,7 @@ $week_later = new DateTime('today + 7 days');
 <html lang='ja'>
        <head>
            <meta charset='UTF-8'>
-           <meta name='viewport' content='width=device-width'>
+           <meta name='viewport' content='width=device-width,initial-scale=1'>
            <meta name='keywords' content='数学、医学部、大学受験、難関大学、プログラミング教育'>
            <title>体験授業</title>
            <link rel='stylesheet' href='styles.css'>
